bd6e4d6c243cf56a1d24ec0f495a1c547e5521a4
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-07-08 15:04:40 -0400

    added required files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import Header from "./components/Header";
+import Portfolio from "./components/Portfolio";
+import ContactMe from "./components/ContactMe";
+import Footer from "./components/Footer";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#428e92",
+      main: "#006064",
+      dark: "#00363a",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      light: "#819ca9",
+      main: "#546e7a",
+      dark: "#29434e",
+      contrastText: "#ffffff",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      TEST
+      {/* <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <br />
+          <Portfolio />
+          <br />
+          <ContactMe />
+          <Footer />
+        </div>
+      </ThemeProvider> */}
+    </Box>
   );
 }
 
 export default App;
+
+// <img width="50px" src={require("../images/Jeremy_duncan_logo_React.webp")}/>
