a469c49af06449dcc6d139a9c7608117f2cb928d
Jeremy Duncan <jduncan46@my.devry.edu>
2022-08-10 21:33:35 -0400

    added line seperators

--- src/components/devry-components/DevryProjects.js
+++ src/components/devry-components/DevryProjects.js
@@ -11,6 +11,7 @@ import "@fontsource/roboto/700.css";
 import NavigateNextRoundedIcon from "@mui/icons-material/NavigateNextRounded";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
@@ -37,6 +38,79 @@ class DevryProjects extends Component {
           justifyContent="center"
           alignItems="center"
         >
+          <Grid
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              variant="h1"
+              color="lightgray"
+              sx={{
+                fontWeight: "500",
+                textAlign: "center",
+                display: { xs: "none", sm: "none", md: "initial" },
+              }}
+            >
+              DeVry Portfolio
+            </Typography>
+            <Typography
+              variant="h4"
+              color="lightgrey"
+              sx={{
+                fontWeight: "500",
+                textAlign: "center",
+                display: { xs: "initial", sm: "initial", md: "none" },
+              }}
+            >
+              DeVry Portfolio
+            </Typography>
+
+            <Typography
+              variant="h4"
+              mb={1}
+              sx={{
+                color: "lightgrey",
+                textAlign: "center",
+                display: { xs: "none", sm: "none", md: "initial" },
+              }}
+            >
+              My Projects From DeVry
+            </Typography>
+            <Typography
+              variant="h7"
+              mb={1}
+              sx={{
+                color: "lightgrey",
+                textAlign: "center",
+                display: { xs: "initial", sm: "initial", md: "none" },
+              }}
+            >
+              My Projects From DeVry
+            </Typography>
+
+            <Grid
+              xs={6}
+              sx={{
+                height: "1px",
+                border: {
+                  xs: "4px solid gray",
+                  sm: "4px solid gray",
+                  md: "8px solid gray",
+                },
+                display: "flex",
+                justifyContent: "center",
+                textAlign: "center",
+                alignItem: "center",
+                margin: "auto",
+                width: "500px",
+                borderRadius: "20px",
+                marginBottom: "3rem",
+              }}
+            />
+          </Grid>
+
           {/*//================== PHYSICS GRID BOX ========================> */}
           <Grid
             sx={{
@@ -130,6 +204,13 @@ class DevryProjects extends Component {
             </Grid>
           </Grid>
           {/*//========================= GRID END =========================> */}
+          <Grid
+            elevation={24}
+            mb={5}
+            ml={1.2}
+            xs={10}
+            sx={{ height: "1px", border: "2px solid gray" }}
+          />
           {/*//============== CLOUD COMOUTING GRID BOX ====================> */}
           <Grid
             sx={{
@@ -222,6 +303,13 @@ class DevryProjects extends Component {
             </Grid>
           </Grid>
           {/*//========================= GRID END =========================> */}
+          <Grid
+            elevation={24}
+            mb={5}
+            ml={1.2}
+            xs={10}
+            sx={{ height: "1px", border: "2px solid gray" }}
+          />
           {/*//========================= IOT GRID BOX =====================> */}
           <Grid
             sx={{
@@ -314,7 +402,204 @@ class DevryProjects extends Component {
               </Item>
             </Grid>
           </Grid>
-          {/*//========================= GRID END ===================================> */}
+          {/*//========================= GRID END =========================> */}
+          <Grid
+            elevation={24}
+            mb={5}
+            ml={1.2}
+            xs={10}
+            sx={{ height: "1px", border: "2px solid gray" }}
+          />
+          {/*//========================= IOT GRID BOX =====================> */}
+          <Grid
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              flexDirection: { xs: "column", sm: "column", md: "row" },
+            }}
+            ml={1}
+            pb={5}
+          >
+            <Grid
+              item
+              xs={10}
+              sm={10}
+              md={6}
+              lg={5.5}
+              xl={3.5}
+              sx={{ pb: { xs: ".5rem", md: "0rem" } }}
+            >
+              <Item
+                sx={{
+                  bgcolor: "primary.secondary",
+                }}
+                elevation={12}
+              >
+                {/*==> Image Link <== ===*/}
+                <Image
+                  src={require("../../images/devry-images/stock_tracker.webp")}
+                  sx={{ borderRadius: "5px" }}
+                />
+              </Item>
+            </Grid>
+
+            <Grid
+              item
+              xs={10}
+              sm={10}
+              md={5}
+              lg={5.5}
+              xl={3.5}
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <Item elevation={12}>
+                <Typography
+                  variant="h5"
+                  p={2}
+                  sx={{
+                    color: "#004787",
+                    textShadow: "1.5px 1.5px 4px lightgrey",
+                    fontWeight: "bold",
+                  }}
+                >
+                  CEIS150 Final Project:
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "#004787",
+                    textShadow: "1.5px 1.5px 4px lightgrey",
+                  }}
+                >
+                  Object Oriented Programming
+                </Typography>
+                <Typography
+                  pl={3}
+                  pr={3}
+                  sx={{
+                    color: "contrast.textDark",
+                    textShadow: "0px 3px 8px #8a8a8a",
+                  }}
+                  align="left"
+                >
+                  This is the final project for my DeVry class CEIS150. In this
+                  project I walk you through a stock tracking program I created
+                  using Python.
+                </Typography>
+                <br />
+                <Button
+                  sx={{ color: "button.textLight" }}
+                  href="/"
+                  variant="contained"
+                  endIcon={<NavigateNextRoundedIcon />}
+                >
+                  View Project
+                </Button>
+                <br />
+                <br />
+              </Item>
+            </Grid>
+          </Grid>
+          {/*//========================= GRID END =========================> */}
+          <Grid
+            elevation={24}
+            mb={5}
+            ml={1.2}
+            xs={10}
+            sx={{ height: "1px", border: "2px solid gray" }}
+          />
+          {/*//=============== Network Security GRID BOX ==================> */}
+          <Grid
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              flexDirection: { xs: "column", sm: "column", md: "row" },
+            }}
+            ml={1}
+            pb={5}
+          >
+            <Grid
+              item
+              xs={10}
+              sm={10}
+              md={6}
+              lg={5.5}
+              xl={3.5}
+              sx={{ pb: { xs: ".5rem", md: "0rem" } }}
+            >
+              <Item
+                sx={{
+                  bgcolor: "primary.secondary",
+                }}
+                elevation={12}
+              >
+                {/*==> Image Link <== ===*/}
+                <Image
+                  src={require("../../images/devry-images/CyberSecurity.webp")}
+                  sx={{ borderRadius: "5px" }}
+                />
+              </Item>
+            </Grid>
+
+            <Grid
+              item
+              xs={10}
+              sm={10}
+              md={5}
+              lg={5.5}
+              xl={3.5}
+              sx={{ display: "flex", alignItems: "center" }}
+            >
+              <Item elevation={12}>
+                <Typography
+                  variant="h5"
+                  p={2}
+                  sx={{
+                    color: "#004787",
+                    textShadow: "1.5px 1.5px 4px lightgrey",
+                    fontWeight: "bold",
+                  }}
+                >
+                  SEC285 Final Project:
+                </Typography>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "#004787",
+                    textShadow: "1.5px 1.5px 4px lightgrey",
+                  }}
+                >
+                  Information Security
+                </Typography>
+                <Typography
+                  pl={3}
+                  pr={3}
+                  sx={{
+                    color: "contrast.textDark",
+                    textShadow: "0px 3px 8px #8a8a8a",
+                  }}
+                  align="left"
+                >
+                  This is the final project for my DeVry class SEC285. In this
+                  project I walk you through information security fundamentals.
+                </Typography>
+                <br />
+                <Button
+                  sx={{ color: "button.textLight" }}
+                  href="/"
+                  variant="contained"
+                  endIcon={<NavigateNextRoundedIcon />}
+                >
+                  View Project
+                </Button>
+                <br />
+                <br />
+              </Item>
+            </Grid>
+          </Grid>
+          {/*//========================= GRID END =========================> */}
         </Grid>
       </Box>
     );
