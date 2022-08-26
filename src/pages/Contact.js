80c7b0e29fa01de2f36bbda834fd26c233618265
Jeremy Duncan <jduncan46@my.devry.edu>
2022-08-25 22:35:51 -0400

    changed import names

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,21 +8,20 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import Box from "@mui/material/Box";
 import React, { Component } from "react";
-import MilitaryContactMe from "../components/military-components/MilitaryContactMe";
-import MilitaryFooter from "../components/military-components/MilitaryFooter";
-import MilitaryHeader from "../components/military-components/MilitaryHeader";
-import MilitaryTraining from "../components/military-components/MilitaryTraining";
+import ContactFooter from "../components/contact-components/ContactFooter";
+import ContactHeader from "../components/contact-components/ContactHeader";
+import ContactInfo from "../components/contact-components/ContactInfo";
+import Header from "../components/Header";
 
 class Contact extends Component {
   render() {
     return (
       <Box>
-        <MilitaryHeader />
+        <Header />
         <br />
         <br />
-        <MilitaryTraining />
-        <MilitaryContactMe />
-        <MilitaryFooter />
+        <ContactInfo />
+        <ContactFooter />
       </Box>
     );
   }
